15e622113632a6d7c73235fca8e43e7df86555e0
pavl_g <scrappers.tm@gmail.com>
2023-07-07 10:35:04 +0300

    refactored license to the AvrSandbox project

--- snaploader/src/main/java/com/avrsandbox/snaploader/package-info.java
+++ snaploader/src/main/java/com/avrsandbox/snaploader/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009-2023 jMonkeyEngine
+ * Copyright (c) 2023, AvrSandbox, jSnapLoader
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
